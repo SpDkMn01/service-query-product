43f68fa78659c8f20cd65ed1b8588f002a5d69b1
Pedro Manuel Diaz Santa Maria <pdiazsan@emeal.nttdata.com>
2022-10-17 08:54:18 -0500

    First commit

---
+++ src/main/java/com/nttdata/bootcamp/project/QueryProduct/QueryProductApplication.java
@@ -0,0 +1,13 @@
+package com.nttdata.bootcamp.project.QueryProduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueryProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueryProductApplication.class, args);
+	}
+
+}
